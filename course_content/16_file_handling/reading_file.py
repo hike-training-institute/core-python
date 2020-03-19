0f0318d5e7def09ee183b3e37c0d828273c69892
Hari <hariharan.hawx@gmail.com>
2020-03-19 09:22:26 +0530

    1st Push

--- course_content/16_file_handling/reading_file.py
+++ course_content/16_file_handling/reading_file.py
@@ -1,19 +1,34 @@
 
-file_obj = open('names.txt' ,mode='a+')
+# Ex - 1
+"""# r - reading the file
+# r+ - read + write
+# w+ - overwriting
+# a+ - append
+
+file_obj = open('names.txt' ,mode='r')
+for i in file_obj.readlines():
+    print(i)
+
+file_obj.write("Hawx\n")
+"""
+
+#Ex - 2
+"""file_obj = open('names.txt' ,mode='a+')
 file_obj1 = open('names.txt' ,mode='a+')
 
-# for i in file_obj.readlines():
-#     print(i)
-
-file_obj.write("Mani Ratnam\n")
 file_obj1.write("Goutham Menon \n")
+file_obj.write("Mani Ratnam\n")
 
-file_obj1.close()
+
 file_obj.close()
+file_obj1.close()
 
-with open('names.txt' ,mode='a+') as f :
+# Order of output depends in the close cmmnd."""
+
+#Ex - 3
+"""with open('names.txt' ,mode='a+') as f :
     f.write("Narendra Modi\n")
-
+"""
 
 
 
