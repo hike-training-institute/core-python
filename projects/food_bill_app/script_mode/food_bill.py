4a7926318ac1cb8f952e03f9bf80b8e6bc7d8a0a
hike-training-institute <hike.traininginstitute@gmail.com>
2020-03-20 09:16:57 +0530

    updated with logging...

--- projects/food_bill_app/script_mode/food_bill.py
+++ projects/food_bill_app/script_mode/food_bill.py
@@ -1,19 +1,24 @@
 from details import food_prices, personal_details
+import logging
+from configs.log_configs import food_bill_logger as f_log
+f_log.setLevel(logging.DEBUG)
 
 def show_billing_options(restaurant):
-    print('\n')
-    print("Here is your hotel menu, Enjoy your meal ....")
+    f_log.warning("Here is your hotel menu, Enjoy your meal ....")
     for i, j in food_prices[restaurant].items():
-        print (i, ':', j)
+        f_log.debug ("{} : {}".format(i, j))
 
 def validate_user(user, pwd):
     for item in personal_details:
         if item["user"] == user and item["pass"] == pwd:
             return item["user"], item["restaurant"]
     else :
+        f_log.warning("Entered Invalid User and Password...")
         return None, None
 
 def get_user_credentials():
+    restaurant = None
+    f_log.warning("Getting user inputs")
     user = input("enter your username : ")
     pwd = input("enter your password : ")
 
@@ -21,11 +26,14 @@ def get_user_credentials():
 
 
 if __name__ == "__main__":
+
     user, pwd= get_user_credentials()
     valid_user, restaurant = validate_user(user, pwd)
 
     if valid_user:
+        f_log.info("User is valid...")
         show_billing_options(restaurant)
 
     else :
-        print("User not found, Try again ...")
+        f_log.warning("User not found, Try again ...")
+
