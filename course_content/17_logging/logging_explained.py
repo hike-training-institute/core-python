a377024211ba9bde7690d86c752e6d9404a3b635
hike-training-institute <hike.traininginstitute@gmail.com>
2020-03-20 08:50:08 +0530

    updated...

--- course_content/17_logging/logging_explained.py
+++ course_content/17_logging/logging_explained.py
@@ -6,20 +6,29 @@ app_log = logging.getLogger('app')
 db_log = logging.getLogger('db')
 
 
-file_handler = RotatingFileHandler('server.log', maxBytes=1024 * 1024 * 100, backupCount=20)
-file_handler.setLevel(logging.CRITICAL)
+file_handler = RotatingFileHandler('server.log', mode='a+', maxBytes=1024 * 1024 * 100, backupCount=20)
+file_handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter("%(asctime)s - [%(filename)s:%(lineno)s - %(funcName)20s() ] - %(levelname)s - %(message)s")
 file_handler.setFormatter(formatter)
 server_log.addHandler(file_handler)
-server_log.warning("server critical")
 
 
-file_handler = RotatingFileHandler('app.log', maxBytes=1024 * 1024 * 100, backupCount=20)
-file_handler.setLevel(logging.CRITICAL)
+file_handler = RotatingFileHandler('app.log', mode='a+', maxBytes=1024 * 1024 * 100, backupCount=20)
+file_handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter("%(asctime)s - [%(filename)s:%(lineno)s - %(funcName)20s() ] - %(levelname)s - %(message)s")
 file_handler.setFormatter(formatter)
 app_log.addHandler(file_handler)
-app_log.warning("app critical")
+
+
+#actual logging ...
+server_log.warning("Hi Dude")
+app_log.warning("welcome Dude")
+server_log.error("Hi error")
+app_log.error("welcome error")
+server_log.critical("Hi critical")
+app_log.critical("welcome critical")
+
+
 
 
 
