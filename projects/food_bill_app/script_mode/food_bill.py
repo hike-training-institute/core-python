1cc8891ce9b507a87e925b84189c8e55e0e46c29
SambitSubhankar <sambitsubhankar22@gmail.com>
2020-03-28 18:44:48 +0530

    Committing food bill impl with maximum login attempts

--- projects/food_bill_app/script_mode/food_bill.py
+++ projects/food_bill_app/script_mode/food_bill.py
@@ -1,24 +1,21 @@
 from details import food_prices, personal_details
-import logging
-from configs.log_configs import food_bill_logger as f_log
-f_log.setLevel(logging.DEBUG)
 
 def show_billing_options(restaurant):
-    f_log.warning("Here is your hotel menu, Enjoy your meal ....")
+    print('\n')
+    print("Here is your hotel menu, Enjoy your meal ....")
     for i, j in food_prices[restaurant].items():
-        f_log.debug ("{} : {}".format(i, j))
+        print (i, ':', j)
 
 def validate_user(user, pwd):
+
     for item in personal_details:
         if item["user"] == user and item["pass"] == pwd:
             return item["user"], item["restaurant"]
     else :
-        f_log.warning("Entered Invalid User and Password...")
         return None, None
 
 def get_user_credentials():
-    restaurant = None
-    f_log.warning("Getting user inputs")
+
     user = input("enter your username : ")
     pwd = input("enter your password : ")
 
@@ -26,14 +23,19 @@ def get_user_credentials():
 
 
 if __name__ == "__main__":
+    def first(count):
+        user, pwd= get_user_credentials()
+        valid_user, restaurant = validate_user(user, pwd)
 
-    user, pwd= get_user_credentials()
-    valid_user, restaurant = validate_user(user, pwd)
-
-    if valid_user:
-        f_log.info("User is valid...")
-        show_billing_options(restaurant)
-
-    else :
-        f_log.warning("User not found, Try again ...")
-
+        if valid_user:
+            show_billing_options(restaurant)
+        else :
+            if count < 3:
+                print("User not found, Try again ...")
+                count+=1
+                first(count)
+            else :
+                print("You have exceeded the maximum 3 number of attempts.....Exiting.......")
+                exit()
+    count =1
+    first (count)
